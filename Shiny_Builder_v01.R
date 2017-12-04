393e2063142f2ba7dc24ae766649f6576f0a7c0c
msisk1 <msisk1@nd.edu>
2017-12-04 16:10:41 -0500

    Geocoding finished and Shiny Started

---
+++ Shiny_Builder_v01.R
@@ -0,0 +1,104 @@
+rm(list=ls(all=TRUE)) # clear memory
+
+packages<- c("maptools","rgdal","leaflet","htmlwidgets","shiny","ggmap") # list the packages that you'll need
+lapply(packages, require, character.only=T) # load the packages, if they don't load you might need to install them first
+setwd("E:/GIT_Checkouts/R_Scripts/ministerial-elopements")
+latlong <- "+init=epsg:4326"
+
+
+#Loading the geocoded data
+elop.raw <- read.csv("ministerial_elopements_geocoded.csv",stringsAsFactors = F)
+
+
+
+
+
+elop.raw$popupw <- paste(sep = "",  "<b>",elop.raw$Full_Name,"</b><br/>",
+                         "Name = ",elop.raw$Accusations,"<br/>",
+                         "Origin = ",elop.raw$Location_Origin,"<br/>",
+                         "Found = ",elop.raw$Location_Found,"<br/>"
+) #A bit of HTML To make the popups on the lines
+
+
+#Creating the Line objects out of the point objects:
+elop.comp <- elop.raw[which(!is.na(elop.raw$Latitude_Found)),]
+lines <- list()
+for (i in 1:nrow(elop.comp)) { 
+  lines[[i]] <- Lines(list(Line(rbind(c(elop.comp$Longtitude_Origin[i],elop.comp$Latitude_Origin[i]), c(elop.comp$Longtitude_Found[i], elop.comp$Latitude_Found[i]) ))), as.character(i)) 
+  #print(i)
+}
+complete.lines <- SpatialLinesDataFrame(SpatialLines(lines),elop.comp)
+
+
+
+#Examples of figures
+b <- rbind(table(elop.raw$Decade),table(elop.raw$Eloped_with_Married_Woman, elop.raw$Decade))
+barplot(b)
+
+#by decade
+
+
+
+#Mapping Section
+
+#converting to point data frames for mapping
+orig.spdf <- elop.raw[which(!is.na(elop.raw$Latitude_Origin)),]
+coordinates(orig.spdf)=~Longtitude_Origin+Latitude_Origin
+proj4string(orig.spdf) <- CRS(latlong)
+
+found.spdf <- elop.raw[which(!is.na(elop.raw$Latitude_Found)),]
+coordinates(found.spdf)=~Longtitude_Found+Latitude_Found
+proj4string(found.spdf) <- CRS(latlong)
+
+
+#Basic Leaflet Maps
+m <- leaflet()
+m %>% addTiles() %>%
+  addPolylines(data = complete.lines, popup = ~popupw, group = "Connections") %>%
+  addMarkers(data = orig.spdf, popup = ~popupw, group = "Origin",clusterOptions = markerClusterOptions()) %>%
+  addMarkers(data = found.spdf, popup = ~popupw, group = "Found",clusterOptions = markerClusterOptions()) %>%
+  addLayersControl(
+    overlayGroups = c("Origin", "Found","Connections"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
+
+
+#Building Shiny Interface
+ui <- fluidPage(
+  leafletOutput("mymap"),
+  p(),
+  sliderInput("range", "Range:",
+              min = 1870, max = 1915,
+              value = c(1870,1915), sep = ""),
+  # selectInput("decade", "Decade:",
+  #             c("all", "1870s","1880s","1890s","1900s","1910-1914")),
+  selectizeInput("denomination", "Denomination:",
+                 choices = c("all", sort(unique(elop.raw$Denomination_for_Tableau))))
+)
+
+server <- function(input, output, session) {
+  
+  points <- eventReactive(c(input$range, input$denomination), {
+    if (input$denomination == "all"){
+      orig.spdf[which(orig.spdf$Year >= input$range[1] & orig.spdf$Year <= input$range[2]),]
+    }else{
+      orig.spdf[which(orig.spdf$Year >= input$range[1] & orig.spdf$Year <= input$range[2] & orig.spdf$Denomination_for_Tableau == input$denomination),]  
+    }
+    
+    
+  }, ignoreNULL = FALSE)
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      fitBounds(-129,24.2,-65.58,50.54)%>%
+      addProviderTiles(providers$Stamen.TonerLite, options = providerTileOptions(noWrap = TRUE)) 
+    # %>% addMarkers(data = points())
+  })
+  observe({
+    leafletProxy("mymap") %>%
+      clearMarkers() %>%  
+      addMarkers(data = points())
+  })
+}
+
+shinyApp(ui, server)
--- gecoding.R
+++ gecoding.R
@@ -3,46 +3,73 @@ rm(list=ls(all=TRUE)) # clear memory
 packages<- c("maptools","rgdal","leaflet","htmlwidgets","shiny","ggmap") # list the packages that you'll need
 lapply(packages, require, character.only=T) # load the packages, if they don't load you might need to install them first
 setwd("E:/GIT_Checkouts/R_Scripts/ministerial-elopements")
+latlong <- "+init=epsg:4326"
 
 
+geocode.it <- FALSE
 
-
-elop.raw <- read.csv("ministerial_elopements.csv",stringsAsFactors = F)
-elop.ba
-
-
-for(i in 1:nrow(elop.raw)){
-  if (is.na(elop.raw$Acc_Origin[i])){
-    result <- tryCatch({
-      geocode(elop.raw$Location_Origin[i], output = "more", source = "google")
-    },warning=function(w){     
+if (geocode.it){
+  #NOTE: This is a fantastically stupid way of doing this. Someday I will change it to a function and make it pretty
+  
+  elop.raw <- read.csv("ministerial_elopements.csv",stringsAsFactors = F)
+  elop.raw$Acc_Found<-NA
+  elop.raw$Longtitude_Found<-NA
+  elop.raw$Latitude_Found<-NA
+  for(i in 1:nrow(elop.raw)){
+    if (is.na(elop.raw$Acc_Origin[i])){ #uses the accuracy variable as a check if it has been done
+      result <- tryCatch({
+        #Try this, if it fails because of something on Googles end, do the other stuff
+        geocode(elop.raw$Location_Origin[i], output = "more", source = "google")
+      },warning=function(w){#in case of a warning     
         result$lon <- NA
         result$lat <- NA
         result$loctype <- "FAILED"
         return(result)
-    }, error = function(e) {      
+      }, error = function(e) { #in case of an error     
         result$lon <- NA
         result$lat <- NA
         result$loctype <- "FAILED"
         return(result)
-    }
+      }#end error
       
-    )
-      
-    elop.raw$Longtitude_Origin[i] <- as.numeric(result$lon)
-    elop.raw$Latitude_Origin[i] <- as.numeric(result$lat)
-    elop.raw$Acc_Origin[i] <- as.character(result$loctype)
-  }
-  
-}
+      )#end trycatch
+      #Then give it all the values
+      elop.raw$Longtitude_Origin[i] <- as.numeric(result$lon)
+      elop.raw$Latitude_Origin[i] <- as.numeric(result$lat)
+      elop.raw$Acc_Origin[i] <- as.character(result$loctype)
+    }#end of the if statement for origin geocoding
+    if (is.na(elop.raw$Acc_Found[i])&elop.raw$Location_Found != ""){ #beginging of if statement for destination geocoding
+          result <- tryCatch({
+            #Try this, if it fails because of something on Googles end, do the other stuff
+                  geocode(elop.raw$Location_Found[i], output = "more", source = "google")
+            },warning=function(w){#in case of a warning     
+                  result$lon <- NA
+                  result$lat <- NA
+                  result$loctype <- "FAILED"
+                  return(result)
+            }, error = function(e) { #in case of an error     
+                  result$lon <- NA
+                  result$lat <- NA
+                  result$loctype <- "FAILED"
+                  return(result)
+            }#end error
+          
+          )#end trycatch
+      #Then give it all the values
+      elop.raw$Longtitude_Found[i] <- as.numeric(result$lon)
+      elop.raw$Latitude_Found[i] <- as.numeric(result$lat)
+      elop.raw$Acc_Found[i] <- as.character(result$loctype)
+    }#end of the if statement
+    
+    
+    
+  }#end of the for loop
+  if (!file.exists("ministerial_elopements_geocoded.csv")){
+    write.csv(elop.raw,"ministerial_elopements_geocoded.csv", row.names = F)
+  }# end if statement in output
+}else{ #else if already geocoded, just read the saved version
+  elop.raw <- read.csv("ministerial_elopements_geocoded.csv",stringsAsFactors = F)
+} 
 
-for(i in 1:nrow(elop.raw)){
-  if (!is.na(elop.raw$Location_Found[i])){
-    result <- tryCgeocode(elop.raw$Location_Origin[i], output = "more", source = "google")
-    elop.raw$Longtitude_Origin[i] <- as.numeric(result$lon)
-    elop.raw$Latitude_Origin[i] <- as.numeric(result$lat)
-    elop.raw$Acc_Origin[i] <- as.character(result$loctype)
-    Sys.sleep(.2)
-  }
-  
-}
+
+
